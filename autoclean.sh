287f4b7dab24e8cf94545215decafa0409dd9a8c
fisted <van.fisted@googlemail.com>
2012-09-02 01:43:33 +0200

    autohell skeleton

---
+++ autoclean.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+make clean
+make distclean
+rm -f Makefile.in
+rm -f aclocal.m4
+rm -rf autom4te.cache/
+rm -rf build-aux
+rm -rf m4
+rm -f common/Makefile.in
+rm -f config.h.in
+rm -f configure
+rm -f src/Makefile.in
+rm -f autoscan*.log
+rm -f configure.scan
---
+++ src/fagbnc.c
@@ -0,0 +1,9 @@
+#if HAVE_CONFIG_H
+# include <config.h>
+#endif
+
+int
+main(void)
+{
+	return 0;
+}
